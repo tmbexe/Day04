51e5edd3ec1201f90d2b5df101a99f75540f59b3
Thamsanqa Mbexe <tmbexe@c4r13s5.wethinkcode.co.za>
2020-06-25 13:03:30 +0200

    Did ex00 but not done

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,15 +6,11 @@
 /*   By: tmbexe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 12:42:20 by tmbexe            #+#    #+#             */
-/*   Updated: 2020/06/25 13:00:57 by tmbexe           ###   ########.fr       */
+/*   Updated: 2020/06/25 13:03:01 by tmbexe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-
-void ft_putchar(const char c){
-	write(1, &c,1);
-}
 
 int multiplyNumbers(int n) {
     if (n >= 1){
@@ -25,8 +21,7 @@ int multiplyNumbers(int n) {
 }
 
 /*int main(){
-	int n;
-	
+	int n;	
 	ft_putchar('Enter a number: ');
 	scanf("%d", n);
 	ft_putchar('Factorial of %d = %d', n, multiplyNumbers(n));
